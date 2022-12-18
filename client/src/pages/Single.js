5ac74070d15e875150454bec57644150142c347f
birata81 <borislav.kostov81@gmail.com>
2022-12-18 13:47:30 +0200

    Making Single Post page

--- client/src/pages/Single.js
+++ client/src/pages/Single.js
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Single = () => {
   return (
-    <div>Single</div>
+    <div className='single'>
+      <div className='content'>
+        <img src='https://images.pexels.com/photos/9433003/pexels-photo-9433003.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load' alt='' />
+        <div className='user'>
+          <img src='https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1600' alt='' />
+          <div className='info'>
+            <span>John</span>
+            <p>Posted 2 days ago</p>
+          </div>
+        </div>
+      </div>
+      <div className='menu'>menu</div>
+    </div>
   )
 }
 
