1fd7743ac1368cd658689ef3b12cca65dbdc34aa
birata81 <borislav.kostov81@gmail.com>
2023-01-01 19:13:19 +0200

    Made the db connection. Installed mysql2 because of an Network Error. Made Routes and controllers for Auth. Made the hash encrypted for the password in the database. Made some test users.

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
 import express from "express";
+import cors from 'cors'
+import postRoutes from './routes/posts.js'
+import authRoutes from './routes/auth.js'
+import userRoutes from './routes/users.js'
 
 const app = express()
-
+app.use(cors())
 app.use(express.json())
+
+app.use('/api/auth/', authRoutes)
 
 app.listen(8800, ()=> {
     console.log('Connected!')
